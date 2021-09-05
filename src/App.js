154e13a55a9a31d0b4472b46d154e613fff727ca
jianylei <jianyonglei@gmail.com>
2021-09-05 14:41:39 -0400

    mmoved state to main

--- src/App.js
+++ src/App.js
@@ -6,42 +6,10 @@ import Footer from "./components/footer"
 import './App.css';
 
 function App() {
-  const[resume, setResume] = useState({
-    personalInfo:{
-      firstName: "",
-      lastName: "",
-      title: "",
-      phone: "",
-      email: "",
-      summary: "",
-      link:[{
-        title: "",
-        url: ""
-      }]
-    },
-    experience:[{
-      id: uniqid(),
-      company: "",
-      position: "",
-      city: "",
-      start: "",
-      end: ""
-    }],
-    education:[{
-      id: uniqid(),
-      instituteName: "",
-      city: "",
-      degree: "",
-      subject: "",
-      start: "",
-      end: ""
-    }]
-  })
-
   return (
     <div>
      <Header />
-     <Main />
+     <Main/>
      <Footer />
     </div>
   );
--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,37 @@ import Form from "./form/form"
 import Preview from "./preview/preview"
 
 function Main() {
+    const[resume, setResume] = useState({
+        personalInfo:{
+          firstName: "",
+          lastName: "",
+          title: "",
+          phone: "",
+          email: "",
+          summary: "",
+          link:[{
+            title: "",
+            url: ""
+          }]
+        },
+        experience:[{
+          id: uniqid(),
+          company: "",
+          position: "",
+          city: "",
+          start: "",
+          end: ""
+        }],
+        education:[{
+          id: uniqid(),
+          instituteName: "",
+          city: "",
+          degree: "",
+          subject: "",
+          start: "",
+          end: ""
+        }]
+      })
     return (
         <div className="main-container">
             <Form />
