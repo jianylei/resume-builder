b08b284bf28564841e4092b4c4b68ab7131e4253
jianylei <jianyonglei@gmail.com>
2021-09-05 14:39:55 -0400

    created state for resume

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import uniqid from "uniqid"
 import Header from "./components/header";
 import Main from "./components/main";
 import Footer from "./components/footer"
 import './App.css';
 
 function App() {
+  const[resume, setResume] = useState({
+    personalInfo:{
+      firstName: "",
+      lastName: "",
+      title: "",
+      phone: "",
+      email: "",
+      summary: "",
+      link:[{
+        title: "",
+        url: ""
+      }]
+    },
+    experience:[{
+      id: uniqid(),
+      company: "",
+      position: "",
+      city: "",
+      start: "",
+      end: ""
+    }],
+    education:[{
+      id: uniqid(),
+      instituteName: "",
+      city: "",
+      degree: "",
+      subject: "",
+      start: "",
+      end: ""
+    }]
+  })
+
   return (
     <div>
      <Header />
